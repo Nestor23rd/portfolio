30ee9502c99a0fcc795d0ea503a78cfcf162c04b
Nestor23rd <kpadjanestor8@gmail.com>
2026-09-23 18:20:51 +0000

    configuration des fichiers de deploiement et debut du deploiement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\View::composer('*', function ($view) {
             try {
                 if (\Illuminate\Support\Facades\Schema::hasTable('site_settings')) {
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias(['admin' => EnsureAdmin::class]);
         $middleware->web(append: [SetLocale::class]);
         $middleware->redirectGuestsTo(fn (Request $request) => route('admin.login'));
